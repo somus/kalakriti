d7ee9c558c57d08adccca914424401e8773989f5
Somasundaram Ayyappan <1802828+somus@users.noreply.github.com>
2025-05-05 10:01:35 +0530

    Add typecheck to lint command

--- src/components/ui/credenza.tsx
+++ src/components/ui/credenza.tsx
@@ -31,6 +31,7 @@ interface BaseProps {
 interface RootCredenzaProps extends BaseProps {
 	open?: boolean;
 	onOpenChange?: (open: boolean) => void;
+	modal?: boolean;
 }
 
 interface CredenzaProps extends BaseProps {
