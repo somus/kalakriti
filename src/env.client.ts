8a196d2ec07349071412a9ade3fcba0a1a861fa9
Somasundaram Ayyappan <1802828+somus@users.noreply.github.com>
2025-07-04 16:41:37 +0530

    Fix env

--- src/env.client.ts
+++ src/env.client.ts
@@ -8,9 +8,10 @@ export const env = createEnv({
 		VITE_CLERK_PUBLISHABLE_KEY: z.string().min(1)
 	},
 	runtimeEnv: {
-		VITE_PUBLIC_SERVER: import.meta.env.VITE_PUBLIC_SERVER as string,
-		VITE_CLERK_PUBLISHABLE_KEY: import.meta.env
-			.VITE_CLERK_PUBLISHABLE_KEY as string
+		VITE_PUBLIC_SERVER: (import.meta.env.VITE_PUBLIC_SERVER ??
+			process.env.VITE_PUBLIC_SERVER) as string,
+		VITE_CLERK_PUBLISHABLE_KEY: (import.meta.env.VITE_CLERK_PUBLISHABLE_KEY ??
+			process.env.VITE_CLERK_PUBLISHABLE_KEY) as string
 	},
 	skipValidation: !!import.meta.env.SKIP_ENV_VALIDATION,
 	emptyStringAsUndefined: true
