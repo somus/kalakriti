a46fe9c26c8e2ed1e2e5f0c4bd7b1e37897974fd
Somasundaram Ayyappan <1802828+somus@users.noreply.github.com>
2025-07-07 12:48:06 +0530

    Delete the whole event if the user is deleting the last sub event of that event

--- shared/db/mutators/subEvents.ts
+++ shared/db/mutators/subEvents.ts
@@ -8,7 +8,20 @@ export function createSubEventMutators(authData: AuthData | undefined) {
 	return {
 		delete: async (tx, { id }: { id: string }) => {
 			assertIsAdmin(authData);
-			await tx.mutate.subEvents.delete({ id });
+			const subEvent = await tx.query.subEvents
+				.where('id', '=', id)
+				.related('event', q => q.related('subEvents'))
+				.one();
+			if (!subEvent) throw new Error('SubEvent not found');
+
+			const isLastSubEvent = subEvent.event?.subEvents?.length === 1;
+
+			if (isLastSubEvent) {
+				// Delete the whole event if it's the last sub-event
+				await tx.mutate.events.delete({ id: subEvent.event.id });
+			} else {
+				await tx.mutate.subEvents.delete({ id });
+			}
 		}
 	} as const satisfies CustomMutatorDefs<Schema>;
 }
